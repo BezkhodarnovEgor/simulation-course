7f8da2f09dd5ed7df463028df5db849e8e8e3d17
BezkhodarnovEgor <bezkhodarnov.yegor@mail.ru>
2026-02-20 13:17:15 +0700

    m

--- lab01/lab1/obj/Debug/net8.0-windows/lab1.AssemblyInfo.cs
+++ lab01/lab1/obj/Debug/net8.0-windows/lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf9bab2160bde8a7c158bcb97763244f5eb48af")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
